97f1bf7d0dce1fa7c074eb8d24c2a45b1c4d12b9
Huy Duy <46210597+pnhuyduy@users.noreply.github.com>
2020-01-16 14:06:23 +0700

    Thêm chức năng sửa link, lấy url từ id

--- app/UrlDatabase.php
+++ app/UrlDatabase.php
@@ -1,5 +1,6 @@
 <?php
     require_once 'configs.php';
+    require_once 'Cache.php';
 
 class UrlDatabase {
     private static $table = 'short_urls';
@@ -14,6 +15,11 @@ class UrlDatabase {
         $stmt = $this->db->prepare("INSERT INTO ". self::$table ."(long_url, short_code, created_at) VALUES (?, ?, now())");
         $stmt->bind_param("ss", $url, $code);
         $stmt->execute();
+        $cache = new Cache;
+        $cache->addData($code, [
+            "longUrl" => $url,
+            "status" => 1,
+          ], 5);
         return $stmt;
     }
 
@@ -26,8 +32,31 @@ class UrlDatabase {
         return (empty($result)) ? false : true;
     }
 
+    public function updateUrlData($id, $longUrl, $shortCode, $status, $expire) {
+        $stmt = $this->db->prepare("UPDATE ". self::$table ." SET long_url = ?, short_code = ?, status = ? WHERE id = ?");
+        $stmt->bind_param("ssii", $longUrl, $shortCode, $status, $id);
+        $stmt->execute();
+        $result = $stmt->affected_rows;
+        $cache = new Cache;
+        $cache->delData($code);
+        $cache->addData($shortCode, [
+            "longUrl" => $longUrl,
+            "status" => $status
+          ], 100);
+        return $result;
+    }
+
+    // Kiểm tra nếu link đã tồn tại
+    public function checkIfShortCodeExists($shortCode) {
+        $stmt = $this->db->prepare("SELECT * FROM ". self::$table ." WHERE short_code = ? LIMIT 1");
+        $stmt->bind_param("s", $shortCode);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+        return (empty($result)) ? false : $result;
+    }
+
     // Get short code từ url
-    public  function getShortCode($url) {
+    public  function getShortCodeByUrl($url) {
         $stmt = $this->db->prepare("SELECT * FROM ". self::$table ." WHERE long_url = ? LIMIT 1");
         $stmt->bind_param("s", $url);
         $stmt->execute();
@@ -35,6 +64,17 @@ class UrlDatabase {
         return (empty($result)) ? false : $result["short_code"];
     }
 
+    // Get Url data từ id
+    public function getUrlData($id) {
+        $stmt = $this->db->prepare("SELECT * FROM ". self::$table ." WHERE id = ? LIMIT 1");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+
+        return (empty($result)) ? false : $result;
+    }
+
+    // Get tất cả url trong database
     public function getShortUrls() {
         $arr = [];
         $stmt = $this->db->prepare("SELECT * FROM ". self::$table);
